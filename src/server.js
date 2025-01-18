264319dbc1f9365fd2ecbb887975ead049ca8504
Dev Saini <devwithsmile007@gmail.com>
2025-01-18 22:20:39 +0530

    cors update

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ process.setMaxListeners(20);
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(requestLogger);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
